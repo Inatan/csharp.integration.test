7dca310c368c42ce574b4b59db2ecba727431130
inatan <inatan_hertzog@hotmail.com>
2020-12-10 12:32:29 -0300

    Mock type

--- CoisasAFazer.Testes/CadastraTarefaHandlerExecute.cs
+++ CoisasAFazer.Testes/CadastraTarefaHandlerExecute.cs
@@ -6,6 +6,7 @@ using Microsoft.EntityFrameworkCore;
 using System;
 using System.Linq;
 using Xunit;
+using Moq;
 
 namespace CoisasAFazer.Testes
 {
@@ -31,6 +32,27 @@ namespace CoisasAFazer.Testes
             //assert
             var tarefa = repo.ObtemTarefas(t => t.Titulo == "Estudar Xunit").FirstOrDefault();
             Assert.NotNull(tarefa);
+        }
+
+        [Fact]
+        public void QuandoExceptionForLancadaResultadoIsSucessDeveSerFalse()
+        {
+            //arrange
+            var comando = new CadastraTarefa("Estudar Xunit", new Categoria("Estudo"), new DateTime(2019, 12, 31));
+
+            var mock = new Mock<IRepositorioTarefas>();
+
+            mock.Setup(r => r.IncluirTarefas(It.IsAny<Tarefa[]>()))
+                .Throws(new Exception("Houve um erro na inclusão de tarefas"));
+            var repo = mock.Object;
+
+            var handler = new CadastraTarefaHandler(repo);
+
+            //act
+            CommandResult resultado = handler.Execute(comando);
+
+            //assert
+            Assert.False(resultado.isSucesss);
         }
     }
 }
